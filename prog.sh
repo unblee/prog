19d68f1c45bdb13f36a87811b5fad5541309ad86
unblee <unblee@users.noreply.github.com>
2017-01-04 19:42:34 +0900

    Fix

--- prog.sh
+++ prog.sh
@@ -3,9 +3,9 @@
 # progress <print text: string> <condition command: string> [<repeat count: int>]
 # e.g. progress.sh "Downloading" "[ ! -e /tmp/file ]" 10
 
-[ ! -z T ] && orig_text=$1 || orig_text=$T
-[ ! -z C ] && condition=$2 || orig_text=$C
-[ ! -z R ] && repeat=$3    || repeat=$R
+[ -z T ] && orig_text=$1 || orig_text=$T
+[ -z C ] && condition=$2 || condition=$C
+[ -z R ] && repeat=$3    || repeat=$R
 [ -z $repeat ] && repeat=3
 
 while `eval ${condition}`
