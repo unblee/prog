78399d05105405754f5dee7e5e8dc3c817983f33
unblee <unblee@users.noreply.github.com>
2017-01-04 22:18:16 +0900

    Fix

--- prog.sh
+++ prog.sh
@@ -3,10 +3,10 @@
 # prog.sh <print text: string> <condition command: string> [<repeat count: int>]
 # e.g. prog.sh "Downloading" "[ ! -e /tmp/file ]" 10
 
-[[ -z $T ]] && orig_text=$1 || orig_text=$T
-[[ -z $C ]] && condition=$2 || condition=$C
-[[ -z $R ]] && repeat=$3    || repeat=$R
-[[ -z $repeat ]] && repeat=3
+[[ -z "$T" ]] && orig_text=$1 || orig_text=$T
+[[ -z "$C" ]] && condition=$2 || condition=$C
+[[ -z "$R" ]] && repeat=$3    || repeat=$R
+[[ -z "$repeat" ]] && repeat=3
 
 while `eval ${condition}`
 do
