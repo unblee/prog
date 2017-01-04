33a6e501d22cdd51e5c3b485b75df6980fe3dc2b
unblee <unblee@users.noreply.github.com>
2017-01-04 19:21:27 +0900

    Initial commit

---
+++ prog.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# progress <print text: string> <condition command: string> [<repeat count: int>]
+# e.g. progress.sh "Downloading" "[ ! -e /tmp/file ]" 10
+
+local orig_text=$1
+local condition=$2
+local repeat
+[ -z $3 ] && repeat=3 || repeat=$3
+
+while `eval ${condition}`
+do
+  for i in `seq ${repeat}`
+  do
+    text="${orig_text}"
+    for j in `seq ${i}`; do text="${text}."; done
+    echo -en "\r" && echo -n ${text}
+    sleep 0.3
+    echo -en "\r" && echo -n ${text} | sed "s/./ /g"
+  done
+done
